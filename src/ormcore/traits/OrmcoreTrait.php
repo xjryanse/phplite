f5a2c621ac320e3254de49d7a0ae208d898dc560
xieJialun <36151536+xjryanse@users.noreply.github.com>
2026-01-19 19:32:19 +0800

    1

--- src/ormcore/traits/OrmcoreTrait.php
+++ src/ormcore/traits/OrmcoreTrait.php
@@ -12,9 +12,20 @@ trait OrmcoreTrait {
      * 核心模型映射的数据表
      * @return type
      */
-    public static function save($data) {
+    public static function save(array $data) {
         $tableName  = static::getTable();
         $res        = DataSdk::tableDataSave($tableName, $data);
+        return $res;
+    }
+    /**
+     * 2026年1月19日
+     * @param array $data
+     * @return type
+     */
+    public function update(array $data){
+        $tableName  = static::getTable();
+        $data['id']          = $this->uuid;
+        $res = DataSdk::tableDataUpdate($tableName, $data);
         return $res;
     }
 
