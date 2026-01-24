b2457df3409a90f15ac22b45fe8cd90b014329a0
xieJialun <36151536+xjryanse@users.noreply.github.com>
2026-01-24 14:51:40 +0800

    1

--- src/ormcore/CoreBaseEntry.php
+++ src/ormcore/CoreBaseEntry.php
@@ -3,15 +3,19 @@ namespace xjryanse\phplite\ormcore;
 
 use xjryanse\phplite\ormcore\OrmCoreBase;
 use xjryanse\servicesdk\data\DataSdk;
-use xjryanse\servicesdk\DbSdk;
-use Exception;
 
 /**
- * 参数透传版本
+ * 入口文件专用版本
  */
-abstract class CoreBasePDown {
+abstract class CoreBaseEntry {
     
     use \xjryanse\phplite\traits\InstMultiTrait;
+
+    public function instInit(){
+        static::commInst($this->uuid);
+    }
+    
+    
     // 调用实际类的方法
     public static function __callStatic($method, $params) {
         $inst = static::commInst();
@@ -25,41 +29,32 @@ abstract class CoreBasePDown {
         return call_user_func_array([$inst, $method], $params);
     }
 
-    protected static $hostBindId;
-    /**
-     * 设定外部绑定id
-     */
-    public static function setHostBind($hostBindId){
-        static::$hostBindId = $hostBindId;
-    }
-    
-    protected static $times = 0;
     /**
      * 覆盖下级inst
      * @param type $id
      * @return type
      */
     public static function commInst($id = 0){
-        static::$times ++;
-        if(static::$times > 10 ){
-            throw new Exception('死循环');
-        }
-        // 设定数据库操作sdk实例
-        $hostBindId = static::$hostBindId;
-        if(!$hostBindId){
-            throw new Exception('未设置$hostBindId');
-        }
-        $dbId       = DbSdk::dbId(static::$dbCate);
-
         $inst       = OrmCoreBase::inst($id);
-        $dataSdk    = DataSdk::inst($hostBindId)->dbBind($dbId);
+        // 此处逻辑不一样，关键直接透传
+        // 入口的，必须写死，不然没地方取
+        // 注意：这里的sdk配置的是数据库中台，不是Entry本身
+        $serverInfo = [
+            'workerman_ip'      =>'127.0.0.1',
+            'workerman_port'    =>'19914',
+            'http_ip'           =>'127.0.0.1',
+            'http_port'         =>'9914',
+        ];
+        $dataSdk    = DataSdk::inst('ENTRY');
+        $dataSdk->serverInfoSet($serverInfo);
         $inst->setDataSdk($dataSdk);
         // 设定操作数据表
         $table      = static::getTable();
         $inst->setTable($table);
         return $inst;
     }
-
+    
+    
     /**
      * 核心模型映射的数据表
      * @return type
