ea42e8bdf6852e4727a1cc03521f639b2ad870ea
xieJialun <36151536+xjryanse@users.noreply.github.com>
2026-02-17 23:29:28 +0800

    Update RqParamsInterface.php

--- src/interfaces/RqParamsInterface.php
+++ src/interfaces/RqParamsInterface.php
@@ -12,11 +12,11 @@ interface RqParamsInterface {
     // 域名
     public function host();
     // 获取请求头
-    public function header(string $key = null);
+    public function header(?string $key = null);
     // 获取GET参数
-    public function get(string $key= null);
+    public function get(?string $key= null);
     // 获取POST参数
-    public function post(string $key= null);
+    public function post(?string $key= null);
     // 标记当前运行环境：fpm；swoole
     public function env();
     
