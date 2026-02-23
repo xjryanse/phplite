bfe8574deb746face0735adaa17a7c3b4bd33461
xieJialun <36151536+xjryanse@users.noreply.github.com>
2026-02-23 22:53:46 +0800

    Update OrmcoreCommTrait.php

--- src/ormcore/coreBase/OrmcoreCommTrait.php
+++ src/ormcore/coreBase/OrmcoreCommTrait.php
@@ -3,6 +3,7 @@
 namespace xjryanse\phplite\ormcore\coreBase;
 
 use xjryanse\phplite\logic\Arrays;
+use xjryanse\phplite\logic\SnowFlake;
 use Exception;
 /**
  * 这里封装控制器通用的更新方法
@@ -27,6 +28,27 @@ trait OrmcoreCommTrait {
         // 2026年2月15日:发现报错：没有指定dataSdk实例？？？
         return static::inst($id)->update($dataFilter);
     }
-    
+    /**
+     * 2026年2月23日6
+     * @param type $param
+     * @return type
+     */
+    public static function commSave($param){
+        $data   = Arrays::value($param, 'table_data') ? : $param;
+        // 获取表有字段
+        $fields     = static::inst()->fields();
+        $dataFilter = Arrays::getByKeys($data, $fields);
+
+        $id = Arrays::value($data, 'id');
+        if($id){
+            // 2026年2月15日:发现报错：没有指定dataSdk实例？？？
+            return static::inst($id)->update($dataFilter);
+        } else {
+            $dataFilter['id'] = SnowFlake::generateParticle();
+            return static::inst()->insert($dataFilter);
+        }
+        
+        
+    }
     
 }
