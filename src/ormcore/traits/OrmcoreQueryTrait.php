511bdd7140989d7b1d5d6952bd67a6dab954514c
xieJialun <36151536+xjryanse@users.noreply.github.com>
2026-05-09 19:28:50 +0800

    Update OrmcoreQueryTrait.php

--- src/ormcore/traits/OrmcoreQueryTrait.php
+++ src/ormcore/traits/OrmcoreQueryTrait.php
@@ -38,10 +38,10 @@ trait OrmcoreQueryTrait {
      * @param type $con
      * @return type
      */
-    public function conList($con = [],$orderBy='', $limit = 0){
+    public function conList($con = [],$orderBy=''){
         $this->dataSdkCheck();
         $tableName              = $this->table;
-        $lists = $this->dataSdk->tableDataConList($tableName, $con, $orderBy, $limit);
+        $lists = $this->dataSdk->tableDataConList($tableName, $con, $orderBy);
         if($orderBy){
             $lists = Arrays2d::sort($lists, $orderBy);
         }
