7cdeadded5d4ca9c74d8ca8b35256bb337c8772e
xieJialun <36151536+xjryanse@users.noreply.github.com>
2026-06-21 14:23:17 +0800

    1

---
+++ src/session/RSession.php
@@ -0,0 +1,19 @@
+<?php
+namespace xjryanse\phplite\session;
+
+use xjryanse\phplite\core\RCache;
+/**
+ * 会话数据；代理门面
+ * @see \xjryanse\phplite\core\RCache
+ */
+class RSession{
+    // 页面配置缓存用3
+    protected static $instIndex = 1;
+
+    // 调用实际类的方法
+    public static function __callStatic($method, $params) {
+        // 运行时，即调用子类
+        $inst   = RCache::inst(static::$instIndex);
+        return call_user_func_array([$inst, $method], $params);
+    }
+}
